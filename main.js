2a9bf6ab36a12a51943bc16bdebc8e25de4d728a
amgillenwater <amgillenwater@gmail.com>
2020-05-07 10:09:56 -0400

    some small adjustments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const headerSection = document.querySelector("#title");
 const infoSection = document.querySelector(".info")
 const picSection = document.querySelector(".pic-bio")
 
-function createImageElement (url) {
+function createImageElement(url) {
     return `<img src="${url}">`
 }
 
@@ -15,30 +15,28 @@ fetch("https://api.github.com/users/amgillenwater")
         console.log(data);
         //the part above is the part I don't really get--once i can console.log and see the data, I know what to do but getting there is trickier
         let nameHeadEl = document.createElement("div")
-        nameHeadEl.className = "header-name"
-        nameHeadEl.innerText = data.name;
-        headerSection.appendChild(nameHeadEl);
+            nameHeadEl.className = "header-name"
+            nameHeadEl.innerText = data.name;
+            headerSection.appendChild(nameHeadEl);
         //leftside div
         let nameEl = document.createElement("div")
-        nameEl.className = "header-name"
-        nameEl.innerText = "Name - " + data.name;
-        infoSection.appendChild(nameEl);
+            nameEl.className = "header-name"
+            nameEl.innerText = "Name - " + data.name;
+            infoSection.appendChild(nameEl);
         let usernameEl = document.createElement("div");
-        usernameEl.className = "info-section"
-        usernameEl.innerText = "Github username -  " + data.login;
-        infoSection.appendChild(usernameEl);
+            usernameEl.className = "info-section"
+            usernameEl.innerText = "Github username -  " + data.login;
+            infoSection.appendChild(usernameEl);
         let companyEl = document.createElement("div");
-        companyEl.innerText = "Company - " + data.company;
-        infoSection.appendChild(companyEl);
+            companyEl.innerText = "Company - " + data.company;
+            infoSection.appendChild(companyEl);
         let websiteEl = document.createElement("div");
-        websiteEl.innerText = "Website - " + data.html_url;
-        infoSection.appendChild(websiteEl);
+            websiteEl.innerText = "Website - " + data.html_url;
+            infoSection.appendChild(websiteEl);
         //pic section
         let bioPhoto = document.createElement("div");
-        bioPhoto.innerHTML = createImageElement(data.avatar_url)
-        picSection.appendChild(bioPhoto);
-
-
+            bioPhoto.innerHTML = createImageElement(data.avatar_url)
+            picSection.appendChild(bioPhoto);
     })
     //it's the two steps above I don't quite get--once I console.log the data I know where to go
 
